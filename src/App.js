e094072775ad872f362cc24d3cc520eef9e662f9
Victor <jvleon95@gmail.com>
2020-06-21 04:40:11 -0500

    include styles for ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import store from './store'
 import { Provider } from 'react-redux'
+import Home from './containers/Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-datepicker/dist/react-datepicker.css'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  .react-datepicker-wrapper,
+  .react-datepicker__input-container,
+  .react-datepicker__input-container input {
+    display: block;
+    width: 100%;
+    border-radius: 4px;
+    border: none
+    outline: none;
+    padding: 3px;
+  }
+  .react-datepicker__input-container input {
+    border: 1px solid #ced4da;
+  }
+  .react-datepicker__input-container input:disabled {
+    background-color: #e9ecef;
+    opacity: 1;
+  }
+`
 
 function App() {
   return (
     <Provider store={store}>
-      <div>Hello world!</div>
+      <Home />
+      <GlobalStyle whiteColor />
     </Provider>
   )
 }
